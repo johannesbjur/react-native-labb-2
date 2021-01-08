7d604682d4563c23161c0a4f61f99179e701c508
johannesbjur <bjurstromerjohannes@gmail.com>
2021-01-08 17:07:52 +0100

    app finished

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './components/header';
+import { Room } from './components/room';
+import { Device } from './components/device';
+
 
 export default function App() {
+
+    const [activeDevices, setActiveDevices] = useState(0)
+    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+        <Header/>
+
+        <Text style={styles.title}>Rooms</Text>
+        <View style={styles.rooms}>
+            <Room
+                title="Living Room"
+                image={ require('./assets/living-room.png') }
+            />
+            <Room
+                title="Bedroom"
+                image={ require('./assets/bed.png') }
+            />
+            <Room
+                title="Kitchen"
+                image={ require('./assets/kitchen.png') }
+            />
+        </View>
+
+        <Text style={styles.title}>Devices</Text>
+        
+        <Device
+            itemName="Living Room Lamp"
+            setActiveDevices={setActiveDevices}
+            activeDevices={activeDevices}
+        />
+
+        <Device
+            itemName="Heater"
+            setActiveDevices={setActiveDevices}
+            activeDevices={activeDevices}
+        />
+
+        <Device
+            itemName="TV"
+            setActiveDevices={setActiveDevices}
+            activeDevices={activeDevices}
+        />
+
+        <Text style={styles.deviceAmount}><Text style={{fontWeight: 'bold'}}>Total Devices On: </Text>{activeDevices}</Text>
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        margin: 10
+    },
+    rooms: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    deviceAmount: {
+        fontSize: 20,
+        marginLeft: 20
+    }
 });
